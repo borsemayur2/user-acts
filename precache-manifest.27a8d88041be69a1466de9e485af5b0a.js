48c66b76275f271375bf239747fc88c3e915fdfc
Mayur Borse <borsemayur2@gmail.com>
2020-09-13 17:47:25 +0530

    Updates

---
+++ precache-manifest.27a8d88041be69a1466de9e485af5b0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6d5d90a734bed73fe2de49ce72d064",
+    "url": "/user-acts/index.html"
+  },
+  {
+    "revision": "e9a59ff7ec737181559a",
+    "url": "/user-acts/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "1394c5480bf1b01d5b52",
+    "url": "/user-acts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9a59ff7ec737181559a",
+    "url": "/user-acts/static/js/2.d7116174.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/user-acts/static/js/2.d7116174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1394c5480bf1b01d5b52",
+    "url": "/user-acts/static/js/main.24e3db67.chunk.js"
+  },
+  {
+    "revision": "5c6d4822bb8755c6b473",
+    "url": "/user-acts/static/js/runtime-main.22a9f462.js"
+  }
+]);
